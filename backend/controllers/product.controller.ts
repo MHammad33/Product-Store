b616694ce3765fd40bd06b5cbabbd7fc732738d0
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-11-01 22:05:28 +0500

    Add utility function to validate product request

--- backend/controllers/product.controller.ts
+++ backend/controllers/product.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import productService from "../services/product.service";
+import { toNewProductType } from "../utils";
 
 const createNewProduct = (req: Request, res: Response) => {
+  const newProduct = toNewProductType(req.body);
   productService.addProduct();
   res.send("Create new Product");
 };
