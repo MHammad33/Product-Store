70596c2cb420e34dad8eec22723c40c008b7cb0d
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-11-01 11:50:35 +0500

    Setup prettier

--- backend/server.ts
+++ backend/server.ts
@@ -5,5 +5,5 @@ const app = express();
 const port = 3001;
 
 app.listen(port, () => {
-	console.log(`Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
