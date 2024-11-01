6da10cac954f72e23be8c728f7ba1f75019b0309
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-11-01 11:52:59 +0500

    Add home route for testing the setup

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+// Routes
+app.get("/", (_req, res) => {
+  res.send("Server Home Page");
+});
+
 const port = 3001;
 
 app.listen(port, () => {
