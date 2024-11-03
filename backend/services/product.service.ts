979a5b1262a2346f7165c7201677cc4d70a02676
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-11-03 18:14:44 +0500

    Fix issue

--- backend/services/product.service.ts
+++ backend/services/product.service.ts
@@ -1,3 +1,10 @@
-const addProduct = () => {};
+import Product from "../models/Product.model";
+import { Product as ProductType } from "../types";
+
+const addProduct = async (product: ProductType): ProductType => {
+  const newProduct = new Product(product);
+  await newProduct.save();
+  return newProduct;
+};
 
 export default { addProduct };
